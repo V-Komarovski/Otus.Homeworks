c083c308656c59189dbe7371ddc8b80754111198
V-Komarovski <vldslv_komarov98@mail.ru>
2025-11-11 09:42:38 +0300

    HW-3

---
+++ Otus.Homeworks/Common.cs
@@ -0,0 +1,56 @@
+﻿namespace Otus.Homeworks;
+
+internal static class FileReader
+{
+    /// <summary>
+    /// Возвращает количество пробелов в файлых указанной директории
+    /// </summary>
+    /// <param name="path">Путь к файлам</param>
+    /// <returns></returns>
+    public static async Task<int> GetWhitespaceCountInDirectory(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentNullException(nameof(path));
+
+        var filePaths = Directory.GetFiles(path);
+
+        var tasks = filePaths.Select(GetFileWhitespaceCount).ToArray();
+
+        var results = await Task.WhenAll(tasks);
+        var whitespaceCount = results.Sum();
+        return whitespaceCount;
+    }
+
+    /// <summary>
+    /// Возвращает количество пробелов в указанном файле
+    /// </summary>
+    /// <param name="localFilePath"></param>
+    /// <returns></returns>
+    private static async Task<int> GetFileWhitespaceCount(string localFilePath)
+    {
+        using var reader = new StreamReader(localFilePath);
+        var content = await reader.ReadToEndAsync();
+        var whitespaceCount = GetWhitespaceCount(content);
+        return whitespaceCount;
+    }
+    
+    /// <summary>
+    /// Возвращает количество пробелов в тексте
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    private static int GetWhitespaceCount(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return 0;
+
+        var count = 0;
+        foreach (var @char in text)
+        {
+            if (@char is ' ')
+                count++;
+        }
+
+        return count;
+    }
+}
---
+++ Otus.Homeworks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Otus.Homeworks;
+
+var filesPath = "Files";
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var whitespaceCount = await FileReader.GetWhitespaceCountInDirectory(filesPath);
+
+stopwatch.Stop();
+
+Console.WriteLine($"Число пробелов: {whitespaceCount}");
+Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+
+Console.ReadKey();
